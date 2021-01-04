4be809b3dbdfa14caf3adc6a5b5024a163ee3b8b
ilvas <il.vsl0110@gmail.com>
2021-01-04 23:22:54 +0300

    pushing template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import client from './client'
+
+import {gql} from "@apollo/client";
+
+
+import {get} from './QUERIES/debug';
+
 
 function App() {
+  client.query({
+    query: gql`${get()}`
+  }).then((data) => {
+    console.log(data)
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world
     </div>
   );
 }

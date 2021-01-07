0ee1eb76318dc3681705b9b8aeb5521f5abfaea6
firesieht <firesieht@mail.ru>
2021-01-08 01:48:57 +0300

    add ChildtestDetail

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {MySelect} from './uiKit/Select';
 import {Conformity, UnDropColumn} from "./uiKit/conformity";
+import {TaskRow, NumRow
+} from "./uiKit/ChildTestDetail"
 import "./uiKit/css/conformity.css"
 
 function click(){
   console.log("походу работает")
 }
 
+// <div className="myDiv">
+// <UnDropColumn answers={["спит","спать", "Петербург","бобра"]}></UnDropColumn>
+// <Conformity answers={["Пошла","Наелся и", "Съел","Санкт"]}></Conformity>
+// </div>,
+
+let chels = [  
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
+]
 ReactDOM.render(
-  <div className="myDiv">
-      <UnDropColumn answers={["спит","спать", "Петербург","бобра"]}></UnDropColumn>
-      <Conformity answers={["Пошла","Наелся и", "Съел","Санкт"]}></Conformity>
-  </div>,
-  
+  <NumRow num={6} taskrows={chels}/>,
   document.getElementById('root')
 );

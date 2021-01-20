bf4fe7644d4cdf3cdaa9c0859e45b4bbc0243fe0
firesieht <firesieht@mail.ru>
2021-01-21 00:29:57 +0300

    add SubjectCard

--- src/uiKit/Cards.tsx
+++ src/uiKit/Cards.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./css/Cards.css"
-import image from "../images/cardImg.svg"
+import "./css/Cards.css";
+import image1 from "../images/cardImg.svg";
+import image2 from "../images/cardImg1.svg";
+import image3 from "../images/cardImg2.svg";
+import image4 from "../images/cardImg3.svg";
+import image5 from "../images/cardImg4.svg";
+
 
 
 interface SubjectConfig{
@@ -9,29 +14,39 @@ interface SubjectConfig{
     subject:string;
     date?:Date;
     newTasks?:number;
+    onClick:Function;
 };
 
 export const SubjectCard : React.FC<SubjectConfig> = (props) => {
+    function getRandomInt(min:number, max:number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min; 
+      }
+
+    let image;
+
+    let images = [image5,image1,image2,image3,image4]
+
+    image = images[getRandomInt(0,images.length)]
     return (
-        <div className="SbCard">
-            <div>
-                <div className="SubjectText">{props.subject}</div>
-                <div className="TeacherText">{props.teacher}</div>
-                <div></div>
-            </div>
-            <div>
-                <p className="TeacherText">
-                    <span className={props.newTasks==undefined? "noTasks":"newTasks"}>{props.newTasks==undefined? "Нет заданий":`Новое задание(${props.newTasks})`}</span><br></br>
-                    {props.newTasks==undefined? "":"до "+ props.date == undefined? "":props.date?.getDate() + "." + props.date?.getMonth()}
-
-                    
-
-                </p>
+        <button className="SbCard" onClick={(event)=>props.onClick(event)}>
+            <div className="padDiv">
+                <div className="subDiv">
+                    <div className="SubjectText">{props.subject}</div>
+                    <div className="TeacherText">{props.teacher}</div>
+                </div>
+                <div className="taskDiv">
+                    <div className={props.newTasks==undefined? "noTasks":"newTasks"}>{props.newTasks==undefined? "Нет заданий":`Новое задание(${props.newTasks})`}</div>
+                    <div className="TeacherText">
+                        {props.newTasks==undefined? "": props.date == undefined? "":"до " + props.date?.getDate() + "." + props.date?.getMonth()}
+                    </div>
+                </div>
             </div>
             <img className="image" src={image}></img>
             
 
-        </div>
+        </button>
     );
 }
 

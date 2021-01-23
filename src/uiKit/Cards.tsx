24c1c7be37557e1f2ad7f1ec196e484c1deeccf2
firesieht <firesieht@mail.ru>
2021-01-24 00:53:58 +0300

    change catd

--- src/uiKit/Cards.tsx
+++ src/uiKit/Cards.tsx
@@ -61,11 +61,13 @@ interface TaskConfig{
     task:Function;
 }
 
+function declOfNum(number:number, words:Array<string>) {  
+    return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+}
+
 export const TaskCard :React.FC<TaskConfig> = (props) => {
 
-    function declOfNum(number:number, words:Array<string>) {  
-        return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
-    }
+
     return(
         <div className="tkCard">
             <span className="SubjectText">{props.subject}</span>
@@ -84,6 +86,42 @@ export const TaskCard :React.FC<TaskConfig> = (props) => {
             </p>
 
             <DefaultButton text="начать выполнение" handleClick={()=>props.task()} style={true}></DefaultButton>
+            
+
+        </div>
+    );
+}
+
+interface GradeConfig{
+    subject:string;
+    theme:string;
+    teacher:string;
+    period?:Date;
+    grade_num?:number;
+    grade_text?:string;
+    exeptions?:number;
+}
+
+export const GradeCard :React.FC<GradeConfig> = (props) => {
+
+    function declOfNum(number:number, words:Array<string>) {  
+        return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+    }
+    return(
+        <div className="tkCard">
+            <span className="SubjectText">{props.subject}</span>
+            <p>
+                <span className="noTasks">{props.theme}</span><br></br>
+                <span className="TeacherText">{props.teacher}</span><br></br>
+                <span className="TeacherText">{props.period==undefined? "не ограничено": "до " + props.period?.getDate() + "." + props.period?.getMonth()+"."+props.period?.getFullYear()+"    "+props.period.getHours()+":"+ props.period.getMinutes()}</span>
+
+
+            </p>
+            <div className="p">
+                <span className="grade">{props.grade_num == undefined? props.grade_text:props.grade_num}</span>
+                <span className="SubjectText" style={{color:"#565656", display:"flex", justifyContent:"flex-end", flexDirection:"column"}}><span style={{display:"block",position:"relative",bottom:"0" }}>{props.exeptions == undefined? "":props.exeptions + declOfNum(props.exeptions, [" ошибка", " ошибки", " ошибок"])}</span></span>
+            </div>
+
             
 
         </div>

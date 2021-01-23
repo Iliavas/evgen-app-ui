2ef6322a3ab117bdfa6dd632b22e5a808dfbf487
firesieht <firesieht@mail.ru>
2021-01-23 20:53:02 +0300

    add TaskCard

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import { from } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {MySelect} from './uiKit/Select';
+import {MySelect, SaveSelect} from './uiKit/Select';
 import {Conformity, UnDropColumn} from "./uiKit/conformity";
-import {TaskRow, NumRow
-} from "./uiKit/ChildTestDetail"
-import "./uiKit/css/conformity.css"
+import {TaskRow, NumRow} from "./uiKit/ChildTestDetail"
+import {MyPagination} from "./uiKit/pagination"
+import {GradeWidget} from "./uiKit/Grade"
+import ReactPaginate from 'react-paginate';
+import {DefaultInput} from './uiKit/Inputs'
+import {DefaultButton} from "./uiKit/Buttons";
+import {SubjectCard, TaskCard} from "./uiKit/Cards"
+import ts from 'typescript';
 
-function click(){
-  console.log("походу работает")
+
+function click(a:number){
+  console.log("походу работает" ,{a})
 }
 
 // <div className="myDiv">
@@ -16,15 +22,34 @@ function click(){
 // <Conformity answers={["Пошла","Наелся и", "Съел","Санкт"]}></Conformity>
 // </div>,
 
-let chels = [  
-<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
-<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
-<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
-<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
-<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
-<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>,
-]
+
+{/* <NumRow num={6}>
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>
+<TaskRow name="Афанасий" surname="Петров" states={[true, false, false, true, false, false]}></TaskRow>
+</NumRow> */}
+
+
+{/* <SaveSelect el={["Выберите предмет","русский","математика","информатика","ты дебил"]}></SaveSelect>, */}
+{/* <div>
+<MyPagination handleClick={()=>click(1)} quantity={10}/>
+</div>, */}
+{/* <DefaultButton text="Войти" handleClick={()=>click(0)}></DefaultButton>, */}
+{/* <SubjectCard date={new Date(2021,1,18)} subject="Математика" onClick={()=>click(1)} teacher="Рыскаль Ебланович" newTasks={5}></SubjectCard>, */}
+
+
 ReactDOM.render(
-  <NumRow num={6} taskrows={chels}/>,
+  <TaskCard
+    subject="Английский язык"
+    theme="Повторение твоей мамы"
+    teacher="Рыскаль ебланович"
+    period={new Date()}
+    questions={1}
+    minuts={42}
+    task={()=>click(0)}
+  ></TaskCard>,
   document.getElementById('root')
 );
--- src/uiKit/Cards.tsx
+++ src/uiKit/Cards.tsx
@@ -6,7 +6,8 @@ import image2 from "../images/cardImg1.svg";
 import image3 from "../images/cardImg2.svg";
 import image4 from "../images/cardImg3.svg";
 import image5 from "../images/cardImg4.svg";
-
+import {DefaultButton} from "./Buttons"
+import { TaskRow } from './ChildTestDetail';
 
 
 interface SubjectConfig{
@@ -50,3 +51,41 @@ export const SubjectCard : React.FC<SubjectConfig> = (props) => {
     );
 }
 
+interface TaskConfig{
+    subject:string;
+    theme:string;
+    teacher:string;
+    period?:Date;
+    questions:number;
+    minuts?:number;
+    task:Function;
+}
+
+export const TaskCard :React.FC<TaskConfig> = (props) => {
+
+    function declOfNum(number:number, words:Array<string>) {  
+        return words[(number % 100 > 4 && number % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+    }
+    return(
+        <div className="tkCard">
+            <span className="SubjectText">{props.subject}</span>
+            <p>
+                <span className="noTasks">{props.theme}</span><br></br>
+                <span className="TeacherText">{props.teacher}</span><br></br>
+                <span className="TeacherText">{props.period==undefined? "не ограничено": "до " + props.period?.getDate() + "." + props.period?.getMonth()+"."+props.period?.getFullYear()+"    "+props.period.getHours()+":"+ props.period.getMinutes()}</span>
+
+
+            </p>
+            <p>
+                <span className="noTasks">{props.questions.toString() + declOfNum(props.questions, [" вопрос"," вопроса"," вопросов"])}</span><br></br>
+                <span className="noTasks">{props.minuts==undefined? "Время на выполнение неограничено": props.minuts.toString() + declOfNum(props.minuts,[' минута', ' минуты', ' минут' + " "])}
+                    {props.minuts==undefined? "":<span className="TeacherText"> на выполнение</span>}
+                </span>
+            </p>
+
+            <DefaultButton text="начать выполнение" handleClick={()=>props.task()} style={true}></DefaultButton>
+            
+
+        </div>
+    );
+}

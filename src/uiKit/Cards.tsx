8704a37ab472d906162237fab2d535a2b7161bed
firesieht <firesieht@mail.ru>
2021-01-24 23:08:49 +0300

    add SubjectGradeCard

--- src/uiKit/Cards.tsx
+++ src/uiKit/Cards.tsx
@@ -127,3 +127,27 @@ export const GradeCard :React.FC<GradeConfig> = (props) => {
         </div>
     );
 }
+
+interface GradeSubject{
+    subject:string;
+    grades:Array<any>
+}
+
+export const GradeSubjectCard : React.FC<GradeSubject> = (props) => {
+
+    let classNames = ["grayGrade","grayGrade","grayGrade","purpleGrade","purpleGrade"]
+    let grades = new Array()
+
+    props.grades.forEach((el:any) => {
+        grades.push(<span className={classNames[el-1]}>{"  "+ el +"   "}</span>)
+      })
+
+    return (
+        <div className="SbCard">
+            <p className="sbp">
+                <span className="SubjectText">{props.subject}</span><br/>
+                {grades}
+            </p>
+        </div>
+    );
+}
